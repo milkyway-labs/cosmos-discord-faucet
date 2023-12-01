f6f2d356dd93ba6c3585dcd5e5e79dd9d3dc3c9b
Fabian Weber <frznhope@gmail.com>
2023-12-01 14:40:27 +0200

    change disclaimer

--- commands/help.js
+++ commands/help.js
@@ -1,4 +1,4 @@
-const millisecondsInHour = 3600000
+const millisecondsInHour = 3600000;
 
 module.exports = () => {
   return `
@@ -20,11 +20,20 @@ $balance ${process.env.ADDRESS_PREFIX}178jjyne475dnh5jpuqfjt30ak4r64xlsxy7xsf
 
 Notes
 
-Users are throttled in requesting ${process.env.AMOUNT}${process.env.DENOMINATION} (${parseFloat((process.env.AMOUNT / 1000000).toFixed(3))} ${process.env.DENOMINATION.slice(1, process.env.DENOMINATION.length).toUpperCase()}) once every ${(process.env.TIMEOUT / millisecondsInHour).toFixed(2)} hours, any other request will not be fulfilled.
+Users are throttled in requesting ${process.env.AMOUNT}${
+    process.env.DENOMINATION
+  } (${parseFloat(
+    (process.env.AMOUNT / 1000000).toFixed(3)
+  )} ${process.env.DENOMINATION.slice(
+    1,
+    process.env.DENOMINATION.length
+  ).toUpperCase()}) once every ${(
+    process.env.TIMEOUT / millisecondsInHour
+  ).toFixed(2)} hours, any other request will not be fulfilled.
 After the throttle expires users can request again.
 
 Created with <3 by 0x4139
 
-USING THIS FAUCET OR RUNNING A TESTNET NODE DOES NOT ENTITLE YOU TO ANY AIRDROP OR OTHER DISTRIBUTION OF MAINNET ${process.env.CHAIN_NAME.toUpperCase()} TOKENS. MAINNET ${process.env.CHAIN_NAME.toUpperCase()} TOKENS DO NOT CURRENTLY EXIST AND THERE ARE NO PUBLIC SALES OR OTHER PUBLIC DISTRIBUTIONS OF ANY MAINNET ${process.env.CHAIN_NAME.toUpperCase()} TOKENS.
-`
-}
+USING THIS FAUCET OR RUNNING A TESTNET NODE DOES NOT ENTITLE YOU TO ANY AIRDROP OR OTHER DISTRIBUTION OF MAINNET MILKYWAY TOKENS. MAINNET MILKYWAY TOKENS DO NOT CURRENTLY EXIST AND THERE ARE NO PUBLIC SALES OR OTHER PUBLIC DISTRIBUTIONS OF ANY MAINNET MILKYWAY TOKENS.
+`;
+};
