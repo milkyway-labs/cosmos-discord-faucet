02c2c516ee9e60f4a09466001393ea98f7b75013
Fabian Weber <frznhope@gmail.com>
2023-12-02 15:01:20 +0200

    verify osmo address

--- commands/request.js
+++ commands/request.js
@@ -1,8 +1,9 @@
-const api = require('../util/api')
-const { CheckIn } = require('../util/registry')
+const api = require("../util/api");
+const { CheckIn } = require("../util/registry");
 
 module.exports = (addr, user) => {
-  CheckIn(addr)
-  CheckIn(user)
-  return api.CosmosTransfer(addr)
-}
+  VerifyAddress(addr);
+  CheckIn(addr);
+  CheckIn(user);
+  return api.CosmosTransfer(addr);
+};
